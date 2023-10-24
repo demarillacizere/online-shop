ee15f2583631331ebf6fbb0a1c2c2ae0b7ee0cf1
demarillacizere <izered3@gmail.com>
2023-10-24 21:37:38 +0200

    docs: Added a database config file

--- src/Views/Main/404.php
+++ src/Views/Main/404.php
@@ -1,6 +1,12 @@
-<?php
-?>
-
-<div class="col">
-    <h1>Page not found!</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h3 class="card-title">Page Not Found!</h3>
+                    <p class="m-0"><a href="<?php echo BASE_URL ?>">Go Back Home</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
